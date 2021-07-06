6ba1aa02e729654bcee0f57c95af82af704a5376
IIVIIIII <kelwalls@gmail.com>
2021-07-05 17:27:53 -0700

    demographic population totals

--- app.js
+++ app.js
@@ -62,10 +62,16 @@ fullCity = city.concat(`, ${state}`)
 countyNums = []
 countyQuery = ''
 tracts = []
-demogs = 'B02001_002E,B02001_003E,B02001_004E,B02001_005E,B02001_006E,B03001_003E'
+pops = 'B02001_002E,B02001_003E,B02001_004E,B02001_005E,B02001_006E,B03001_003E'
 
-// console.log(fullCity)
-// console.log(state)
+demogs = {
+  white: {},
+  black: {},
+  native: {},
+  asian: {},
+  pacific: {},
+  hispanic: {}
+}
 
 d3.csv("resources/ccvi.csv").then(function(data) {
 
@@ -85,31 +91,32 @@ d3.csv("resources/ccvi.csv").then(function(data) {
           else {countyQuery = countyQuery.concat(`,${co}`)}
         })
       
-      d3.json(`https://api.census.gov/data/2019/acs/acs5?get=NAME,${demogs}&for=tract:*&in=county:${countyQuery}&in=state:${stateNum}`).then(function(cenTracts) {
+      d3.json(`https://api.census.gov/data/2019/acs/acs5?get=NAME,${pops}&for=tract:*&in=county:${countyQuery}&in=state:${stateNum}`).then(function(cenTracts) {
         cenTracts.forEach(t => {
           if (t[0] != 'NAME') {
 
             properTract = d => {return d.FIPS == t[7].concat(t[8],t[9])}
 
-            tracts.push(
-              {
-                tract: data.filter(properTract)[0].FIPS,
-                // state: data.filter(properTract)[0].stateName,
-                // county: data.filter(properTract)[0].countyName,
-                ccvi: parseFloat(data.filter(properTract)[0].ccvi),
-                whitePop: parseInt(t[1]),
-                blackPop: parseInt(t[2]),
-                nativePop: parseInt(t[3]),
-                asianPop: parseInt(t[4]),
-                pacificPop: parseInt(t[4]),
-                hispanicPop: parseInt(t[6])
-              })
-          
+            tracts.push({
+              tract: data.filter(properTract)[0].FIPS,
+              // state: data.filter(properTract)[0].stateName,
+              // county: data.filter(properTract)[0].countyName,
+              ccvi: parseFloat(data.filter(properTract)[0].ccvi),
+              white: parseInt(t[1]),
+              black: parseInt(t[2]),
+              native: parseInt(t[3]),
+              asian: parseInt(t[4]),
+              pacific: parseInt(t[4]),
+              hispanic: parseInt(t[6])
+            })
           }
         })
 
-        console.log(tracts)
+        Object.keys(demogs).forEach(key => {
+          demogs[key].population = d3.sum(tracts.map(t => {return t[key]}))
+        })
 
+        console.log(demogs)
 
       })
     })
