902a8b6f8c83c7d046ea36fa9e55af7521fdbb20
IIVIIIII <kelwalls@gmail.com>
2021-08-05 14:13:18 -0500

    heroku files in one folder maybe?

--- heroku/app.py
+++ heroku/app.py
@@ -5,22 +5,14 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    # data = data4app.get_data('california')
-
-    # return jsonify(data)
-
     return "Lets goooo!!!"
+
 
 @app.route("/<var>")
 def jsonified(var):
     data = data4app.get_data(var)
-
     return jsonify(data)
-
-
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-# import sys
-# print(f'\n{sys.prefix}\n')
